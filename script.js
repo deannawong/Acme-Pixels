7c24c3c55402639a7937fbae874fa4540409a954
Raymond Ng <raymond.ng47@gmail.com>
2019-10-07 21:12:51 -0400

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+const grid=document.getElementById('grid');
+
+const addRow=document.getElementById('addRow');
+const removeRow=document.getElementById('removeRow');
+const addRow=document.getElementById('addRow')
+const addRow=document.getElementById('addRow')
